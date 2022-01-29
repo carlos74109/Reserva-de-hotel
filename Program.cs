13914df387bb07114255815d953de99ccb678314
Carlos do Vale <70588116+carlos74109@users.noreply.github.com>
2022-01-29 11:45:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using DesafioTryCath.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioTryCath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero do quarto: ");
+            int numeroQuarto = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Data do check-in (DD/MM/YYYY)");
+            DateTime chekin = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Data do chek-out");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+            if(checkout <= chekin)
+            {
+                Console.WriteLine("Erro, a data do check-out, tem que ser posterior ao check-in");
+            }else
+            {
+                Reserva reserva = new Reserva(numeroQuarto, chekin, checkout);
+                Console.WriteLine(reserva);
+
+                Console.WriteLine();
+                Console.WriteLine("Entre com os dados para atualizar a reserva: ");
+
+                Console.WriteLine("Data do check-in (DD/MM/YYYY)");
+                chekin = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Data do chek-out");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if(chekin < now || checkout < now)
+                {
+                    Console.WriteLine("Erro na reserva, tem que ser datas futuras");
+                }else if(checkout <= chekin)
+                {
+                    Console.WriteLine("Erro na reserva: A reserva tem que ser datas futuras ");
+                }
+                else
+                {
+                    reserva.updateDatas(chekin, checkout);
+                    Console.WriteLine(reserva);
+                }
+
+            }
+
+
+
+        }
+    }
+}
---
+++ Reserva.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioTryCath.Entidade
+{
+    internal class Reserva
+    {
+        public int numeroQuarto;
+        public DateTime chekin;
+        public DateTime chekout;
+
+        public Reserva()
+        {
+
+        }
+
+        public Reserva(int numeroQuarto, DateTime chekin, DateTime chekout)
+        {
+            this.numeroQuarto = numeroQuarto;
+            this.chekin = chekin;
+            this.chekout = chekout;
+        }
+
+        public int Duracao()
+        {
+            TimeSpan duracao = chekout.Subtract(chekin);
+            return (int)duracao.TotalDays;
+        }
+
+        public void updateDatas(DateTime chekin, DateTime checkout)
+        {
+            this.chekin = chekin;
+            chekout = checkout;
+        }
+
+        public override string ToString()
+        {
+            return "Quarto "
+                + numeroQuarto
+                + chekin.ToString(" dd/MM/YYYY")
+                + ", chek-out: "
+                + chekin.ToString(" dd/MM/YYYY")
+                + ", "
+                + Duracao()
+                + " noites";
+        }
+    }
+}
